847ba4b7b9da6ad3480ed0079eed8e176f3d20a8
FelipeAguiarCode <felipe.silva.aguiar047@gmail.com>
2023-02-28 23:39:15 -0300

    ajustado formato geral

--- src/chat-client.tsx
+++ src/chat-client.tsx
@@ -7,6 +7,7 @@ import Grid from '@mui/material/Grid';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import Paper from '@mui/material/Paper';
+import borders from '@mui/system';
 
 interface Props {
   isConnected: boolean;
@@ -25,16 +26,31 @@ export const ChatClient = (props: Props) => {
       position: 'absolute',
       width: '100%',
       height: '100%',
-      backgroundColor: 'white',
+      backgroundColor: '#EDEDEF',
       display: 'flex',
       alignItems: 'center',
     }}>
       <CssBaseline />
-      <Container maxWidth="lg" style={{ height: '90%' }}>
-        <Grid container style={{ height: '100%' }}>
+      <Container 
+        maxWidth="lg" 
+        style={{ height: '90%' }}
+        >
 
+        <Grid container style={{ height: '100%' }}>
           {/** Grid com os contatos conectados */}
-          <Grid item xs={2} style={{ backgroundColor: '#232F3E', color: 'white' }}>
+          <Grid 
+            item xs={2} 
+            sx={{
+              borderTopLeftRadius: '10px',
+              borderBottomLeftRadius: '10px',  
+            }}
+            style={{ 
+              backgroundColor: '#2D46C9',
+              color: 'white',
+              fontFamily: 'segoe UI'
+            }}
+            >
+
             <List component="nav">
               {props.members.map(item =>
                 <ListItem key={item} onClick={() => {{props.onPrivateMessage(item)};}} button>
@@ -42,8 +58,10 @@ export const ChatClient = (props: Props) => {
                 </ListItem>
               )}
             </List>
+
           </Grid>
           
+
           {/* Grid das mensagens enviadas  */}
           <Grid style={{ position: 'relative' }} item container direction="column" xs={10} >
             <Paper style={{ flex: 1 }}>
@@ -62,7 +80,12 @@ export const ChatClient = (props: Props) => {
 
                 {/* Botões de envio de mensagem */}
                 <Grid item style={{ margin: 10 }}>
-                  {props.isConnected && <Button style={{ marginRight: 7, color: "#232F3E", borderColor: "#232F3E" }} variant="outlined" size="small" disableElevation onClick={props.onPublicMessage}>Mensagem pública</Button>}
+                  {props.isConnected && 
+                  
+                  <Button 
+                    style={{ marginRight: 7, color: "#232F3E", borderColor: "#232F3E" }} variant="outlined" size="small" disableElevation onClick={props.onPublicMessage}>Mensagem pública</Button>
+                  }
+                  
                   {props.isConnected && <Button style={{ marginRight: 7, color: "#232F3E", borderColor: "#232F3E" }} variant="outlined" size="small" disableElevation onClick={props.onBotMessage}>Mensagem para o Bot</Button>}
                   {props.isConnected && <Button style={{ marginRight: 7, color: "#232F3E", borderColor: "#232F3E" }} variant="outlined" size="small" disableElevation onClick={props.onDisconnect}>Sair</Button>}
                   {!props.isConnected && <Button style={{ marginRight: 7, color: "#232F3E", borderColor: "#232F3E" }} variant="outlined" size="small" disableElevation onClick={props.onConnect}>Entrar</Button>}
