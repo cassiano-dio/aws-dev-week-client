b8285afb523e0eda7a5ff5c7a5ba770322f0fc16
cassiano-dio <cassiano@dio.me>
2023-03-02 19:41:59 -0300

    Endpoint added

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css';
 import React, { useState, useEffect, useCallback, useRef } from 'react'
 import { ChatClient } from './chat-client';
 
-const URL = '';
+const URL = 'wss://09xqig1wrd.execute-api.us-east-2.amazonaws.com/production/';
 
 const App = () => {
   const socket = useRef<WebSocket | null>(null);
